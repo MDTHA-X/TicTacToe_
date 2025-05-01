a9a158511a18d9333272e6934b64ad8e1f652b34
MDTHA-X <tanjimxyt@gmail.com>
2025-05-01 16:24:14 +0600

    Initial commit

---
+++ src/mainframe.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class mainframe extends JFrame {
+
+    mainframe()
+    {
+        this.setSize(1280,720);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(new Color(0x123456));
+
+
+    }
+}
